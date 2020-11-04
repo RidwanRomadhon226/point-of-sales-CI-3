930eefa9db01c7dcba253034ed38fa27c42d91d7
Ridwan Romadhon <ridwanromadhon48@gmail.com>
2020-11-04 22:25:34 +0700

    Add User Data + Form Validation Implementation

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -15,6 +15,20 @@ class User extends CI_Controller
 
   public function add()
   {
-    $this->template->load('template', 'user/user_add');
+    $this->load->library('form_validation');
+
+    $this->form_validation->set_rules('username', 'Username', 'trim|required|is_unique[user.username]');
+    $this->form_validation->set_rules('name', 'name', 'trim|required|min_length[3]');
+    $this->form_validation->set_rules('password', 'Password', 'required|min_length[3]');
+    $this->form_validation->set_rules('passconf', 'Password Confirmation', 'required|matches[password]');
+    $this->form_validation->set_rules('alamat', 'alamat', 'required');
+    $this->form_validation->set_rules('level', 'level', 'required');
+
+
+    if ($this->form_validation->run() == FALSE) {
+      $this->template->load('template', 'user/user_add');
+    } else {
+      echo 'oke';
+    }
   }
 }
--- application/views/user/user_add.php
+++ application/views/user/user_add.php
@@ -10,3 +10,56 @@
     <li class="active">Tambah User</li>
   </ol>
 </section>
+
+<section class="content">
+  <div class="row">
+    <div class="col-md-6">
+      <div class="box">
+        <div class="box-header">
+          <h3 class="box-title">Tamah Data</h3>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+          <?php echo validation_errors(); ?>
+          <form action="" method="post">
+            <div class="form-group">
+              <label for="exampleInputEmail1">Username</label>
+              <input type="text" class="form-control" id="username" name="username" placeholder="Enter username">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Nama</label>
+              <input type="text" class="form-control" id="name" name="name" placeholder="Enter Name">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Password</label>
+              <input type="password" class="form-control" id="password" name="password" placeholder="Enter Password">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Password Confirmation </label>
+              <input type="password" class="form-control" id="passconf" name="passconf" placeholder="Enter Password Confirmation">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Alamat</label>
+              <input type="text" class="form-control" id="alamat" name="alamat" placeholder="Enter Alamat">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Level User</label>
+              <select name="level" id="level" class="form-control">
+                <option value="">Pilih Level User</option>
+                <option value="1">Admin</option>
+                <option value="2">User</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <button type="submit" class="btn btn-success btn-flat">Save</button>
+              <button type="Reset" class="btn btn-warning btn-flat">Reset</button>
+            </div>
+          </form>
+        </div>
+        <!-- /.box-body -->
+      </div>
+      <!-- /.box -->
+
+    </div>
+  </div>
+</section>
